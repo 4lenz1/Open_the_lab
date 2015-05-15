a13e262b2c71a0abade2531b022f4f78a6e3a18f
4lenz1 <pingfallenzone@gmail.com>
2015-05-16 03:24:05 +0800

    add function
    add auto/sop lock Fn

--- app/src/main/java/com/example/fallenzone/openthedoor/MainActivity.java
+++ app/src/main/java/com/example/fallenzone/openthedoor/MainActivity.java
@@ -6,6 +6,7 @@ import android.os.Bundle;
 import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
 import android.webkit.CookieManager;
 import android.webkit.CookieSyncManager;
 import android.webkit.JsResult;
@@ -16,6 +17,7 @@ import android.webkit.WebChromeClient;
 import android.content.Context;
 import android.app.AlertDialog;
 import android.content.DialogInterface;
+import android.widget.Button;
 import android.widget.TextView;
 import android.graphics.Bitmap;
 import android.widget.Toast;
@@ -29,7 +31,9 @@ public class MainActivity extends ActionBarActivity {
     private String username = "M5B01";
     private String pwd = "c@dla3";
     private String url = "http://163.25.117.185/OGWeb/LoginForm.aspx";
-
+    private boolean ctrlLock = false ;
+    private  boolean stopOrAuto ;
+    private Button btnAutoLock , btnStopAutoLock;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -38,6 +42,9 @@ public class MainActivity extends ActionBarActivity {
         textView = (TextView) findViewById(R.id.textView);
         webView = (WebView) findViewById(R.id.webView);
         txtUrl = (TextView) findViewById(R.id.txtUrl);
+
+        btnAutoLock = (Button) findViewById(R.id.btnAutoLock);
+        btnStopAutoLock = (Button) findViewById(R.id.btnStopAutoLock);
 
         webView.getSettings().setCacheMode(WebSettings.LOAD_NO_CACHE);
         webView.getSettings().setJavaScriptEnabled(true);
@@ -68,13 +75,73 @@ public class MainActivity extends ActionBarActivity {
 
         webView.setWebViewClient(new WebViewClient());
 
+        //Auto lock click event
+        final String urlCtrlLock = "http://163.25.117.185/OGWeb/OGWebGuard/OGDoorLatchActionPage.aspx" ;
+        btnAutoLock.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                // Perform action on click
+                webView.loadUrl(urlCtrlLock);
+                ctrlLock = true;
+                stopOrAuto = true;
+            }
+        });
+        btnStopAutoLock.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                // Perform action on click
+                webView.loadUrl(urlCtrlLock);
+                ctrlLock = true;
+                stopOrAuto = false;
+            }
+        });
+
 
         webView.setWebChromeClient(new WebChromeClient() {
             public void onProgressChanged(WebView view, int progress) {
                 Log.v("Progress", Integer.toString(progress));
                 if (progress == 100) {
                     txtUrl.setText( webView.getUrl());
-                    if (webView.getUrl().equals("http://163.25.117.185/OGWeb/LoginForm.aspx?ReturnUrl=%2fOGWeb%2fOGWebGuard%2fOGDOutActionPage.aspx")) {
+                    if (ctrlLock == true){
+                        if (webView.getUrl().equals("http://163.25.117.185/OGWeb/LoginForm.aspx?ReturnUrl=%2fOGWeb%2fOGWebGuard%2fOGDOutActionPage.aspx")) {
+                            // Login
+                            view.loadUrl("javascript:var x = document.getElementById('UserAccount').value = '" +
+                                    username + "';");
+                            view.loadUrl("javascript:var y = document.getElementById('UserPassword').value = '" +
+                                    pwd + "';");
+                            view.loadUrl("javascript:(function(){" +
+                                    "l=document.getElementById('LoginBtn');" +
+                                    "e=document.createEvent('HTMLEvents');" +
+                                    "e.initEvent('click',true,true);" +
+                                    "l.dispatchEvent(e);" +
+                                    "})()");
+                            textView.setText("Login");
+                        }
+                    }else if ( webView.getUrl().equals("http://163.25.117.185/OGWeb/Default.aspx")){
+                        txtUrl.setText( webView.getUrl());
+                        webView.loadUrl("http://163.25.117.185/OGWeb/OGWebGuard/OGDoorLatchActionPage.aspx");
+                    }else if ( webView.getUrl().equals("http://163.25.117.185/OGWeb/OGWebGuard/OGDoorLatchActionPage.aspx")) {
+                        txtUrl.setText( webView.getUrl());
+                        if (stopOrAuto == true) {
+                            view.loadUrl("javascript:(function(){" +
+                                    "l=document.getElementById('OGDoorLatchActionCtrl_DeviceLatchOnList_ctl00_DeviceOnBtn_Button');" +
+                                    "e=document.createEvent('HTMLEvents');" +
+                                    "e.initEvent('click',true,true);" +
+                                    "l.dispatchEvent(e);" +
+                                    "})()");
+                            textView.setText("ctrl Click Auto");
+                        }else{
+                            view.loadUrl("javascript:(function(){" +
+                                    "l=document.getElementById('OGDoorLatchActionCtrl_DeviceLatchOffList_ctl00_DeviceOffBtn_Button');" +
+                                    "e=document.createEvent('HTMLEvents');" +
+                                    "e.initEvent('click',true,true);" +
+                                    "l.dispatchEvent(e);" +
+                                    "})()");
+                            textView.setText("ctrl Click Stop");
+                        }
+                        ctrlLock = false;
+                    }
+
+
+                    if (webView.getUrl().equals("http://163.25.117.185/OGWeb/LoginForm.aspx?ReturnUrl=%2fOGWeb%2fOGWebGuard%2fOGDOutActionPage.aspx") && ctrlLock == false) {
                         // Login
                         view.loadUrl("javascript:var x = document.getElementById('UserAccount').value = '" +
                                 username + "';");

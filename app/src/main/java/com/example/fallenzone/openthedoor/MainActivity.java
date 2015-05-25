ca0279d5681d29f6d1b4ef3cf8dd12996e70ac2b
4lenz1 <pingfallenzone@gmail.com>
2015-05-25 16:47:51 +0800

    0.2.0.5
    * bug fixed

--- app/src/main/java/com/example/fallenzone/openthedoor/MainActivity.java
+++ app/src/main/java/com/example/fallenzone/openthedoor/MainActivity.java
@@ -1,6 +1,7 @@
 package com.example.fallenzone.openthedoor;
 
 import android.app.Activity;
+import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.Menu;
